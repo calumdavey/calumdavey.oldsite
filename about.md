766dd8e9daa50870f903562416c98b0a539fc6ed
Calum Davey <calumdavey@Calums-MacBook-Air.local>
2019-04-24 15:38:57 +0100

    Update Abouts

--- about.md
+++ about.md
@@ -3,10 +3,10 @@ layout: page2
 title: About
 ---
 
-I have been working as an epidemiologist at the [London School of Hygiene and Tropical Medicine](https://www.lshtm.ac.uk) since  2011, on projects relating to HIV, sex work, stepped-wedge trials, education, and mental health. I'm one of the deputy directors of the [Centre for Evaluation](http://evaluation.lshtm.ac.uk/). Recently, I've worked with the [Education Endowment Foundation](https://educationendowmentfoundation.org.uk/), and with the [Centre of Excellence for Development Impact and Learning (CEDIL)](https://cedilprogramme.org/).
+I have been working as an epidemiologist at the [London School of Hygiene and Tropical Medicine](https://www.lshtm.ac.uk) since  2011, on projects relating to HIV, sex work, stepped-wedge trials, education, and mental health. I'm one of the deputy directors of the [Centre for Evaluation](http://evaluation.lshtm.ac.uk/). Recently, I've worked with the [Education Endowment Foundation](https://educationendowmentfoundation.org.uk/), and with the [Centre of Excellence for Development Impact and Learning (CEDIL)](https://cedilprogramme.org/). My current position is with the [PENDA](https://www.lshtm.ac.uk/research/centres-projects-groups/penda) project, learning about interventions to improve inclusion of people with disabilities in development projects. 
 
 I'm interested in the evaluation of interventions for public health and development. I try to focus on interventions that address up-stream social determinants of health and wellbeing. I'm still trying to develop a systems perspective, both for the interventions being evaluated but also the way we view wellbeing and how policy decisions are made. 
 
 I teach on an MSc course on Evaluation methods that I co-founded with my supervisor, James Hargreaves. For more information see my [LSHTM profile](https://www.lshtm.ac.uk/aboutus/people/davey.calum), [Google Scholar account](https://scholar.google.co.uk/citations?user=pm4eXUEAAAAJ&hl=en), or view my [CV](files/cv.pdf) (pdf).
 
-Please feel free to contact me at [calum.davey@lshtm.ac.uk](calum.davey@lshtm.ac.uk).
+Please feel free to contact me at [calum.davey@lshtm.ac.uk](calum.davey@lshtm.ac.uk). I sporadically waste an afternoon pointlessly arguing on [Twitter](https://twitter.com/CalumDavey?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor), and otherwise re-tweeting links and comments from people more informed or hilarious than me. Get in touch!
--- files/PENDA/DW3/PENDA deworm sim.R
+++ files/PENDA/DW3/PENDA deworm sim.R
@@ -1,42 +1,119 @@
-# PENDA Deworm3 simulated precision estimates 
+# Set the design parameters for the study 
 
-n <- 2000 # 2000 disabled children, 2000 non-disabled 
-d <- matrix(,nrow=n*2, ncol=4) # Create data 
-d[,1] <- rep(c(0,1),times=c(n,n)) # Code disabled, and non-disabled (1,0)
-c <- 40 # total number of clusters 
-d[,2] <- rep(rep(seq(1,c,1), times=rep(n/c, times=c)),times=2)
+library(openxlsx)
+library(dplyr)
+# Load the number of people enumerated in each cluster 
+N <- openxlsx::read.xlsx('Cluster Population CE1.xlsx')
+N[,3] <- round(N[,2]*(2000/sum(N[,2])),0) # back-calc for the proportion of CWD in each cluster 
+N[,4] <- round(N[,2]*(2000/(sum(N[,2]*0.05))),0) # back-calc for the total children in each cluster 
+colnames(N) <- c('cluster','N','N_cwd','N_c')
+head(N)
 
+# Number of simulations to run 
+j <- 1000
+# Matrix to save the results 
+d <- matrix(, ncol=0, nrow=j)
+
+es <- c(.4,.5,.6,.7,.75)
+
+for (i in es){
 # Set assummed population-level parameters 
-e0_p <- c(.7) # enrolment of non-disabled children 
-e1_p <- c(.5) # enrolment of disabled children 
-k <- c(.01) # Set intercluster coefficient of variation
-var_b1 <- k/e1_p
-var_b2 <- k/e0_p
-# Fix true cluster-level means of the outcome 
-e1_c <- rnorm(40, e1_p, var_b1^.5)
-e0_c <- rnorm(40, e0_p, var_b2^.5)
-d[,3] <- c(rep(e0_c, times=rep(n/c, times=c)),
-           rep(e1_c, times=rep(n/c, times=c)))
-  # Do need a way to avoid values >1 or <0
+# Enrolment 
+e0 <- c(.75) # enrolment of all children 
+e1 <- c(i) # enrolment of disabled children 
+k <- c(.01) # Set intercluster coefficient of variation in enrollment levels 
+var_b0 <- k/e0 # Calculate the variance of the cluster proportions enrolled  
+var_b1 <- k/e1 # Calculate the variance of the cluster proportions enrolled 
+# Fix true cluster-level means of school enrollment 
+rnorm_0_1 <- function(p, v){
+  r <- 1
+  while (r>=1 | r<=0){
+    r <- round(rnorm(1, p, v^.5),2)
+  }
+  return(r)
+}
+
+N$e_0 <- apply(N, 1, function(x) rnorm_0_1(e0, var_b0))
+N$e_1 <- apply(N, 1, function(x) rnorm_0_1(e1, var_b1))
+
+# True relationship between enrollment and treatment in school-treatment arm
+a0 <- c(.9) # 90% of enrolled children are in school and treated 
+a1 <- c(.8) # 80% of enrolled children with disabilities are in school and treated 
+# True proportions treated by the intervention
+k <- c(.01) # Set intercluster coefficient of variation in attendance if enrolled 
+var_b0 <- k/a0 # Calculate the variance of the cluster attendance if enrolled 
+var_b1 <- k/a1 # Calculate the variance of the cluster attendance if enrolled   
+# Fix the true cluster-level means of the attendance at school if enrolled 
+N$a_0 <- apply(N, 1, function(x) rnorm_0_1(a0, var_b0))
+N$a_1 <- apply(N, 1, function(x) rnorm_0_1(a1, var_b1))
+
+# Allocate half clusters to treatment and half to control  
+N$allocation <- sample(c(rep(0,length.out=(nrow(N)/2)),rep(1,length.out=nrow(N)/2)))
+
+# Proportion treated in each cluster == 
+# School-only arm: proportion enrolled * proportion in school if enrolled 
+N$t_0 <- N$e_0 * N$a_0 # all children
+N$t_1 <- N$e_1 * N$a_1 # children with disabilities 
+# Community arm: set at 98% with small se
+t_comm <- .98
+var_t_comm <- k/t_comm
+N$t_1[N$allocation == 1] <- apply(N[N$allocation == 1,], 1, function(x) rnorm_0_1(t_comm, var_t_comm))
+N$t_0[N$allocation == 1] <- apply(N[N$allocation == 1,], 1, function(x) rnorm_0_1(t_comm, var_t_comm))
 
 # Generate enrolment outcome 
+# Number of treated children overall, and number of treated disabled children 
 i <- 1
-while (i<=1000){
-  d <- cbind(d, apply(d, 1, function(x) sample(c(0,1),size=1,prob=c(1-x[3],x[3]))))
+t_0 <- matrix(, ncol=0, nrow=nrow(N))
+t_1 <- matrix(, ncol=0, nrow=nrow(N))
+while (i<=j){
+  t_0 <- cbind(t_0, apply(N, 1, function(x)
+    sum(sample(c(0,1),size=x[2],prob=c(1-x['t_0'],x['t_0']),replace=T))))
+  t_1 <- cbind(t_1, apply(N, 1, function(x)
+    sum(sample(c(0,1),size=x[3],prob=c(1-x['t_1'],x['t_1']),replace=T))))
   i <- i+1
 }
 
+## Conduct analysis on simulated data 
+# Produce proportions treated in CWD and in children without disabilities 
+# First need to subtract the number od disabled children treated from the number of non-disabled children treated 
+t_0 <- t_0 - t_1
+# And the denominators 
+N_0 <- N[,2] - N[,3]
+
+# Calculate the proportions 
+t_0_p <- t_0 / N_0
+t_1_p <- t_1 / N[,3] 
+
+# Append the allocation 
+t_0_p <- cbind(t_0_p, N$allocation)
+t_1_p <- cbind(t_1_p, N$allocation)
+
+# Disparity between all children and CWD
+diff_p <- t_0_p[,1:(ncol(t_0_p)-1)] - t_1_p[,1:(ncol(t_1_p)-1)]
+diff_p <- cbind(diff_p, N$allocation)
+
 # Calculate sample enrolment figures 
-p0 <- apply(d[d[,1]==0, 5:ncol(d)], 2, mean)
-p1 <- apply(d[d[,1]==1, 5:ncol(d)], 2, mean)
-diff <- apply(cbind(p0,p1),1,function(x) x[1]-x[2])
+p0_sc   <- as.data.frame(t_0_p) %>% filter(V1001==0) %>% select(-V1001) %>% apply(2, mean)
+p1_sc   <- as.data.frame(t_1_p) %>% filter(V1001==0) %>% select(-V1001) %>% apply(2, mean)
+p0_comm <- as.data.frame(t_0_p) %>% filter(V1001==1) %>% select(-V1001) %>% apply(2, mean)
+p1_comm <- as.data.frame(t_1_p) %>% filter(V1001==1) %>% select(-V1001) %>% apply(2, mean)
 
-# Plot the results 
-hist(p0, xlim=c(0.1,.8),ylim=c(0,50), main='',
-     xlab='R or RD', freq=F)
-hist(p1, add =T, freq=F)
-hist(diff, xlab = 'RD',add=T, freq=F)
-lines(density(diff),col='red')
+# Median disparity in each arm
+diff_sc <- as.data.frame(diff_p) %>% filter(V1001==0) %>% select(-V1001) %>% apply(2, mean)
+diff_comm <- as.data.frame(diff_p) %>% filter(V1001==1) %>% select(-V1001) %>% apply(2, mean)
+
+# Difference in the disparity between the arms 
+diff_in_disp <- diff_sc - diff_comm
+
+d <- cbind(d,diff_in_disp)
+}
 
 
-
+# Plot the differences in the disparities s
+plot(density(d[,1]),col='red',ylim=c(0,30), main='', xlab='Difference in disparity', 
+     freq=F, xlim=c(0,.4),bty="n")
+lines(density(d[,2]), col='red')
+lines(density(d[,3]), col='red')
+lines(density(d[,4]), col='red')
+lines(density(d[,5]), col='red')
+text(x=apply(d,2,mean), y=rep(25, times=ncol(d)), labels = as.character(es*100))
